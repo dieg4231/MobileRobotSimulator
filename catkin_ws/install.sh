ecad69e14079f713b30bf7df697ca5f3ebb3c642
dieg4231 <dieg4231@gmail.com>
2020-07-02 11:42:19 -0500

    pacakge for ubuntu 18

--- catkin_ws/install.sh
+++ catkin_ws/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get  install ros-kinetic-turtlebot-*
+sudo apt-get  install ros-melodic-turtlebot-*
 sudo apt-get install libcgal-qt5-dev libcgal-dev 
 find . -name "*.py" | xargs chmod +x
 
