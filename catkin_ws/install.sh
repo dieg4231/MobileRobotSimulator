dd3f3754480de4a974ba12131e8ab0f8c47638a7
Luis Nava <levacarrillo@gmail.com>
2022-04-21 17:57:25 -0500

    Added keyboard teleop installer into install.sh

--- catkin_ws/install.sh
+++ catkin_ws/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get install ros-melodic-turtlebot-*
+sudo apt-get install ros-melodic-teleop-twist-keyboard
 sudo apt-get install libcgal-qt5-dev libcgal-dev 
 sudo apt-get install ros-melodic-joy
 find . -name "*.py" | xargs chmod +x
